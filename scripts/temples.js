6f6b4d0460de67c648651046cbf9a987f8af0764
samuelakibon <sakibon@byupathway.edu>
2026-07-12 20:59:22 +0100

    go

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,24 @@
 // SELECT THE DOM ELEMENTS FOR OUTPUT
 const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
+const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector("#menu")
+
+
 
 // use the date object
 
 const today = new Date();
 currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
- 
-
-
-
-
 document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
+
+
+// The hamburger menu
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
 
 
 
